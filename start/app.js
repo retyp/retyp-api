81a5bc0e0659354ea2bd5910dd0f43d5cf61fdac
Arthur Dufour <dufourarthur.perso@gmail.com>
2020-01-03 12:51:27 +0100

    chore(deps): added @adonisjs/http-logger provider for access-logs

--- start/app.js
+++ start/app.js
@@ -15,7 +15,8 @@ const providers = [
   '@adonisjs/bodyparser/providers/BodyParserProvider',
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/lucid/providers/LucidProvider',
-  '@adonisjs/redis/providers/RedisProvider'
+  '@adonisjs/redis/providers/RedisProvider',
+  '@adonisjs/http-logger/providers/LoggerProvider'
 ]
 
 /*
