e7448b93ab7404568417f40bad0bc919f9f37a77
Arthur Dufour <dufourarthur.perso@gmail.com>
2020-01-09 20:53:49 +0100

    chore(deps): added @adonisjs/vow w/ default configuration (tests)

---
+++ test/functional/paste-create.spec.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// const { test } = use('Test/Suite')('Paste Create')
+
+// test('should test that content is required', async ({ client }) => {
+//   const response = await client
+//     .post('admin/users')
+//     .send({
+//       name: 'Not a valid paste :('
+//     })
+//     .end()
+
+//   response.assertStatus(400)
+//   response.assertJSONSubset({
+//     errors: [{
+//       source: { pointer: 'content' },
+//       title: 'required'
+//     }]
+//   })
+// })
